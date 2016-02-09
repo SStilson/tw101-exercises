f0624260745805192be11b625fc6a46c06fb71f2
SStilson <sstilson@hmc.edu>
2016-02-08 22:18:30 -0800

    Finished Exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,18 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    int count;
+
+    Accumulator() {
+        count = 0;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public Integer total() {
+        return count;
+    }
 }
