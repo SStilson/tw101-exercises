bb596d2aa7573550f5b255567a0a1a9b7d20babc
SStilson <sstilson@hmc.edu>
2016-02-09 21:52:18 -0800

    Partway through Exercise 9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,39 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
-import java.util.List;
+import java.util.*;
 
 public class Node {
     private String name;
+    private Node left;
+    private Node right;
 
     public Node(String name) {
         this.name = name;
+        this.left = null;
+        this.right = null;
     }
 
     public void add(String nameOfNewNode) {
+        if(this.name == null) {
+            new Node(nameOfNewNode);
+        } else {
+            if(nameOfNewNode.compareTo(this.name) < 0) {
+                this.left.add(nameOfNewNode);
+            } else {
+                this.right.add(nameOfNewNode);
+            }
+        }
     }
 
     public List<String> names() {
-        return null;
+        List<String> namesList = new ArrayList<String>();
+        if(this.name == null) {
+            return namesList;
+        } else {
+            namesList.addAll(this.left.names());
+            namesList.add(this.name);
+            namesList.addAll(this.right.names());
+        }
+        return namesList;
     }
 }
